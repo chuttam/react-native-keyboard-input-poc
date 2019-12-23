5fbee6e6445d92d6192dc136f31450695cd3bb23
Amit Uttam <changingrainbows@gmail.com>
2019-12-23 11:01:29 -0300

    Working demo (RN 0.61.5)

--- App.js
+++ App.js
@@ -1,114 +1,202 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React from 'react';
+import React, { Component } from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
-  View,
   Text,
-  StatusBar,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  PixelRatio,
+  Platform,
+  TextInput,
 } from 'react-native';
+import PropTypes from 'prop-types';
+// import {AutoGrowingTextInput} from 'react-native-autogrow-textinput';
+import {KeyboardAccessoryView, KeyboardUtils} from 'react-native-keyboard-input';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import './demoKeyboards';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
+const IsIOS = Platform.OS === 'ios';
+const TrackInteractive = true;
+
+export default class KeyboardInput extends Component {
+  static propTypes = {
+    message: PropTypes.string,
+  };
+
+  constructor(props) {
+    super(props);
+    this.keyboardAccessoryViewContent = this.keyboardAccessoryViewContent.bind(this);
+    this.onKeyboardItemSelected = this.onKeyboardItemSelected.bind(this);
+    this.resetKeyboardView = this.resetKeyboardView.bind(this);
+    this.onKeyboardResigned = this.onKeyboardResigned.bind(this);
+
+    this.state = {
+      customKeyboard: {
+        component: undefined,
+        initialProps: undefined,
+      },
+      receivedKeyboardData: undefined,
+    };
+  }
+
+  onKeyboardItemSelected(keyboardId, params) {
+    const receivedKeyboardData = `onItemSelected from "${keyboardId}"\nreceived params: ${JSON.stringify(params)}`;
+    this.setState({receivedKeyboardData});
+  }
+
+  onKeyboardResigned() {
+    this.resetKeyboardView();
+  }
+
+  getToolbarButtons() {
+    return [
+      {
+        text: 'show1',
+        testID: 'show1',
+        onPress: () => this.showKeyboardView('KeyboardView', 'FIRST - 1 (passed prop)'),
+      },
+      {
+        text: 'show2',
+        testID: 'show2',
+        onPress: () => this.showKeyboardView('AnotherKeyboardView', 'SECOND - 2 (passed prop)'),
+      },
+      {
+        text: 'reset',
+        testID: 'reset',
+        onPress: () => this.resetKeyboardView(),
+      },
+    ];
+  }
+
+  resetKeyboardView() {
+    this.setState({customKeyboard: {}});
+  }
+
+  showKeyboardView(component, title) {
+    this.setState({
+      customKeyboard: {
+        component,
+        initialProps: {title},
+      },
+    });
+  }
+
+  keyboardAccessoryViewContent() {
+    return (
+      <View style={styles.keyboardContainer}>
+        <View style={{borderTopWidth: StyleSheet.hairlineWidth, borderColor: '#bbb'}}/>
+
+        <View style={styles.inputContainer}>
+          <TextInput
+            maxHeight={200}
+            style={styles.textInput}
+            ref={(r) => {
+              this.textInputRef = r;
+            }}
+            placeholder={'Message'}
+            underlineColorAndroid="transparent"
+            onFocus={() => this.resetKeyboardView()}
+            testID={'input'}
+          />
+          <TouchableOpacity style={styles.sendButton} onPress={() => KeyboardUtils.dismiss()}>
+            <Text>Action</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={{flexDirection: 'row'}}>
+          {
+            this.getToolbarButtons().map((button, index) =>
+              <TouchableOpacity
+                onPress={button.onPress}
+                style={{paddingLeft: 15, paddingBottom: 10}}
+                key={index}
+                testID={button.testID}
+              >
+                <Text>{button.text}</Text>
+              </TouchableOpacity>)
+          }
+        </View>
+      </View>
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+
         <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
+          contentContainerStyle={styles.scrollContainer}
+          keyboardDismissMode={TrackInteractive ? 'interactive' : 'none'}
+        >
+          <Text style={styles.welcome}>{this.props.message ? this.props.message : 'Keyboards example'}</Text>
+          <Text testID={'demo-message'}>{this.state.receivedKeyboardData}</Text>
         </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+
+        <KeyboardAccessoryView
+          renderContent={this.keyboardAccessoryViewContent}
+          onHeightChanged={IsIOS ? height => this.setState({keyboardAccessoryViewHeight: height}) : undefined}
+          trackInteractive={TrackInteractive}
+          kbInputRef={this.textInputRef}
+          kbComponent={this.state.customKeyboard.component}
+          kbInitialProps={this.state.customKeyboard.initialProps}
+          onItemSelected={this.onKeyboardItemSelected}
+          onKeyboardResigned={this.onKeyboardResigned}
+          revealKeyboardInteractive
+        />
+      </View>
+    );
+  }
+}
+
+const COLOR = '#F5FCFF';
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    backgroundColor: COLOR,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  scrollContainer: {
+    justifyContent: 'center',
+    padding: 15,
+    flex: 1,
   },
-  body: {
-    backgroundColor: Colors.white,
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    paddingTop: 50,
+    paddingBottom: 50,
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 25,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  keyboardContainer: {
+    ...Platform.select({
+      ios: {
+        flex: 1,
+        backgroundColor: COLOR,
+      },
+    }),
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+  textInput: {
+    flex: 1,
+    marginLeft: 10,
+    marginTop: 10,
+    marginBottom: 10,
+    paddingLeft: 10,
+    paddingTop: 2,
+    paddingBottom: 5,
+    fontSize: 16,
+    backgroundColor: 'white',
+    borderWidth: 0.5 / PixelRatio.get(),
+    borderRadius: 18,
   },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  sendButton: {
+    paddingRight: 15,
+    paddingLeft: 15,
+    alignSelf: 'center',
   },
 });
-
-export default App;
